3e866f5db086aa025b058164a1813ad5c9205840
ChikhalkarS <Shubhamchikhalkar25@gmail.com>
2024-05-13 17:52:28 +0530

    #intialCommit [Shubham] setup

---
+++ src/main/java/com/byteclub/kdmc/KdmcApplication.java
@@ -0,0 +1,13 @@
+package com.byteclub.kdmc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KdmcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KdmcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/byteclub/kdmc/KdmcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.byteclub.kdmc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KdmcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
